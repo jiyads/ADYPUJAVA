6a9960f18aec00ef0db873126c45876bfaba7f2a
jiyads <45309253+jiyads@users.noreply.github.com>
2020-02-07 13:14:32 +0530

    Add files via upload

---
+++ jiyad1/src/string/StringDemo2.java
@@ -0,0 +1,12 @@
+package string;
+
+public class StringDemo2 {
+	public static void main(String[] args){
+		String j="jiyad";
+		String j1="jiyad";
+		
+		System.out.println("s: "+j.indexOf("d"));
+		System.out.println("s: "+j1.substring(0,3));
+	}
+
+}
